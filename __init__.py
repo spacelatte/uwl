55b954e9cb079050e46c6d81b93cec52bb403561
Mert Akengin <5755154+spacelatte@users.noreply.github.com>
2023-08-30 14:11:27 +0200

    add aws lambda support (#1)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass, is_dataclass, asdict
+from typing import Type
+
+
+def dataclass_to_dict(cls) -> dict:
+	if is_dataclass(cls):
+		return asdict(cls)
+	return cls.__dict__
+
+
+from . import aws, middleware, route, wsgi
+
 __all__ = [
+	"aws",
+	"middleware",
 	"route",
 	"wsgi",
 ]
+
--- example.py
+++ example.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
 
-import route, wsgi
+import re
+
+from . import model, route, wsgi
+
 
 entrypoint = wsgi.app
 #entrypoint = lambda *args, **kwargs: wsgi.app(*args, **kwargs)
 
 @route.set(None, r"^/$")
-def main(env, match, *args, **kwargs):
-	return (200, {}, "main page, unsupported method")
+def home(http: model.HttpRequest, match: re.Match, *args, **kwargs):
+	return (200, {}, "home page, any unsupported method")
 
 @route.set("GET", r"^/$")
-def get(env, match, *args, **kwargs):
+def get(http: model.HttpRequest, match: re.Match, *args, **kwargs):
 	return (200, {}, "hello world!")
 
 @route.set([ "PUT", "POST" ], r"^/$")
-def putpost(env, match, *args, **kwargs):
+def putpost(http: model.HttpRequest, match: re.Match, *args, **kwargs):
 	return (200, {}, {
 		"message": "data you sent is here",
-		"data": env["body"].decode(),
+		"data": http.body,
 	})
 
 @route.set(None, r"^/([^/]+)$")
-def subpath(env, match, *args, **kwargs):
-	return (200, {}, list(match.groups()))
+def subpath(http: model.HttpRequest, match: re.Match, *args, **kwargs):
+	return (200, {}, match.groups())
 
 @route.set(None, [ r"^", r"sub", r"sub$" ])
-def subsub(env, match, *args, **kwargs):
+def subsub(http: model.HttpRequest, match: re.Match, *args, **kwargs):
 	return (200, {}, "array sub")
 
-@route.set(None, [ r"^", r"users", r"([0-9]+)$" ])
-def regexsub(env, match, *args, **kwargs):
-	return (200, {}, list(match.groups()))
+@route.set(None, [ r"^", r"users", r"(?P<user>[0-9]+)$" ])
+def regexsub(http: model.HttpRequest, match: re.Match, *args, **kwargs):
+	return (200, {}, dict(as_list=match.groups(), as_dict=match.groupdict()))
 
 @route.set(None, [ r"^", r"([a-z]+)", r"tags", r"([0-9]+)$" ])
-def regexsubsub(env, match, *args, **kwargs):
-	return (200, {}, list(match.groups()))
+def regexsubsub(http: model.HttpRequest, match: re.Match, *args, **kwargs):
+	return (200, {}, match.groups())
 
-print("some routes for you:", list(route.__routes.keys()))
+print("some routes for you:", list(route.get(None)))
+
+print(route.handle(route.model.HttpRequest(method="GET", path="/tags")))
